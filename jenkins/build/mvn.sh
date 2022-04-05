3949e01ccc08cbad2541819721da84352b5ac276
aswin219 <74107290+aswin219@users.noreply.github.com>
2022-04-05 11:54:47 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,7 +3,6 @@
 echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
-
-WORKSPACE=/home/qburst/JENKINS/jenkins-resources/jenkins/pipeline
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
