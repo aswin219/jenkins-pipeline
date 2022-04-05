d36b709e5eb96ebd019cfaa546e1af8d2ea789af
aswin219 <74107290+aswin219@users.noreply.github.com>
2022-04-05 12:26:43 +0530

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -4,9 +4,6 @@ echo "********************"
 echo "** Pushing image ***"
 echo "********************"
 
-IMAGE="maven-project"
-USER="aswinmkolathur"
-PASS="123454321"
 
 echo "** Logging in ***"
 docker login -u $USER -p $PASS
